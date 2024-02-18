b607a421837ac3aa5472a85fc23f5821e592d4e6
Vishal S Murali <116289447+VykSI@users.noreply.github.com>
2024-02-18 18:31:03 +0530

    Update adminloginPage.vue

--- src/components/adminlogin/adminloginPage.vue
+++ src/components/adminlogin/adminloginPage.vue
@@ -65,7 +65,7 @@ export default {
   },
   methods: {
     registerUser(){
-        axios.post('http://localhost:3000/register', {
+        axios.post('https://beacon-server.vercel.app/register', {
         username: this.username,
         password: this.password,
         name:this.name,
@@ -86,7 +86,7 @@ export default {
   
     loginUser() {
         console.log(this.lusername);
-     axios.post('http://localhost:3000/login', {
+     axios.post('https://beacon-server.vercel.app/login', {
     username: this.lusername,
     password: this.lpassword,
   })
